b0adeeca5fbabbf597742141470bdc17ddc712fa
moinahmad64 <146588574+moinahmad64@users.noreply.github.com>
2023-10-01 17:27:37 +0500

    update

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img src="" alt="No Profile Photo" width="100" height="150"  ></img>
+      <h1>Moin Ahmad</h1>
+      <h4>Web Developer </h4>
+      <p>I have completed my bachelor's degree in information technology. I am hardworking and responsible person. Currently i'm looking for
+        an opputunity where i can learn and enhance my skills and work better for my organization.</p>
+      <hr />
+      <h4>Contact 0322 294 6464</h4>
+      <h4>91 Tulip commercial, ParkView City, Lahore</h4>
+      <h4>moin.moinahmad64@gmail.com</h4>
+      <hr/>
+      <h2>Education</h2>
+      <ul>
+        <li>Matric  2017</li>
+        <li>intermediate  2017</li>
+        <li>BS-IT  2021</li>
+      </ul>
+      <h2>Certificates</h2>
+      <ul>
+        <li>MS Office</li>
+        <li>Social Media Marketing</li>
+      </ul>
+
+
+
+
+    </div>);
+}
+
+export default App;
